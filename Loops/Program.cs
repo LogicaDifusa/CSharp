88702339e57ed58088d2d503c4bd84dc82d06c45
Angel Brambila <malthael.91@gmail.com>
2020-08-17 19:10:22 -0500

    Added Loops Project

---
+++ Loops/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region While
+                int x = 1;
+                while( x <= 11)
+                {
+                    WriteLine(x);
+                    x++;
+                }
+            #endregion
+
+            #region doWhile
+                // string password = string.Empty;
+
+                // do
+                // {
+                //     Write("Enter your password : ");
+                //     password = ReadLine();
+                // }
+                // while ( password != "Pa$$w0rd");
+                // WriteLine("Correct!");
+            #endregion
+
+            #region For
+                for (int y = 0; y <= 10; y++)
+                {
+                    WriteLine(y);
+                }
+
+                // Infinite Loop
+                // for(;;)
+                // {
+                //     WriteLine("Unlimited Power!!!!");
+                // }
+            #endregion
+
+            #region ForEach
+            string [] names = { "George", "Saul", "Charles", "Sergy" };
+            foreach (string name in names)
+            {
+                WriteLine ($"{name} has {name.Length} characters.");
+            }
+            #endregion
+        }
+    }
+}
